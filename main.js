763940859117da4f22c5684a89def6f9220459fd
Andrés Castillo <andres.castillo@acastillo.co>
2022-12-27 20:15:50 -0500

    moves, collitions

--- main.js
+++ main.js
@@ -2,12 +2,27 @@ let snake = [0, 1, 2];
 const size = 10;
 const box = document.getElementById('snake-box');
 const playButton = document.getElementById('play-button');
+const downButton = document.getElementById('down-button');
+const upButton = document.getElementById('up-button');
+const leftButton = document.getElementById('left-button');
+const rightButton = document.getElementById('right-button');
 const interval = 1000;
+let accumulator = 1;
 let divs;
 let idInterval;
 
 playButton.addEventListener('click', () => {
   startGame();
+});
+upButton.addEventListener('click', () => {
+  up();
+});
+downButton.addEventListener('click', down);
+leftButton.addEventListener('click', () => {
+  left();
+});
+rightButton.addEventListener('click', () => {
+  right();
 });
 
 function createBox() {
@@ -27,7 +42,7 @@ function drawSnake() {
 function moveSnake() {
   const tail = snake.shift();
   divs[tail].classList.remove('snake');
-  const head = snake[snake.length - 1] + 1;
+  const head = snake[snake.length - 1] + accumulator;
   if (isCollision(head)) {
     alert('game over');
     clearGame();
@@ -39,7 +54,12 @@ function moveSnake() {
 
 function isCollision(index) {
   console.log(index % size);
-  if (index % size === 0) {
+  if (
+    index >= size * size
+    || index < 0
+    || (accumulator === 1 && index % size === 0)
+    || (accumulator === -1 && (index + 1) % size === 0)
+  ) {
     return true;
   }
   return false;
@@ -55,9 +75,26 @@ function startGame() {
 function clearGame() {
   snake = [0, 1, 2];
   box.innerHTML = '';
+  accumulator = 1;
   clearInterval(idInterval);
   createBox();
   drawSnake();
+}
+
+function up() {
+  accumulator = -size;
+}
+
+function down() {
+  accumulator = size;
+}
+
+function left() {
+  accumulator = -1;
+}
+
+function right() {
+  accumulator = 1;
 }
 
 clearGame();
